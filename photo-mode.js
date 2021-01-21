5676d1a4b775062b306a78a3daf62f4aa2fb9b74
sanctifyZhan <40678049+sanctifyZhan@users.noreply.github.com>
2021-01-21 16:13:23 +0900

    Add files via upload

--- photo-mode.js
+++ photo-mode.js
@@ -1,40 +1,40 @@
 AFRAME.registerComponent('photo-mode', {
-    init: function () {
-        const container = document.getElementById('photoModeContainer')
-        const image = document.getElementById('photoModeImage')
-        const shutterButton = document.getElementById('shutterButton')
-        const closeButton = document.getElementById('closeButton')
+  init: function() {
+    const container = document.getElementById('photoModeContainer')
+    const image = document.getElementById('photoModeImage')
+    const shutterButton = document.getElementById('shutterButton')
+    const closeButton = document.getElementById('closeButton')
 
-        // Container starts hidden so it isn't visible when the page is still loading
-        container.style.display = 'block'
+    // Container starts hidden so it isn't visible when the page is still loading
+    container.style.display = 'block'
 
-        closeButton.addEventListener('click', () => {
-            container.classList.remove('photo')
-        })
+   @ closeButton.addEventListener('click', () => {
+      container.classList.remove('photo')
+    })
 
-        shutterButton.addEventListener('click', () => {
-            // Emit a screenshotrequest to the xrweb component
-            this.el.sceneEl.emit('screenshotrequest')
+    shutterButton.addEventListener('click', () => {
+      // Emit a screenshotrequest to the xrweb component
+      this.el.sceneEl.emit('screenshotrequest')
 
-            // Show the flash while the image is being taken
-            container.classList.add('flash')
-        })
+      // Show the flash while the image is being taken
+      container.classList.add('flash')
+    })
 
-        this.el.sceneEl.addEventListener('screenshotready', e => {
-            // Hide the flash
-            container.classList.remove('flash')
+    this.el.sceneEl.addEventListener('screenshotready', e => {
+      // Hide the flash
+      container.classList.remove('flash')
 
-            // If an error occurs while trying to take the screenshot, e.detail will be empty.
-            // We could either retry or return control to the user
-            if (!e.detail) {
-                return
-            }
+      // If an error occurs while trying to take the screenshot, e.detail will be empty.
+      // We could either retry or return control to the user
+      if (!e.detail) {
+        return
+      }
 
-            // e.detail is the base64 representation of the JPEG screenshot
-            image.src = 'data:image/jpeg;base64,' + e.detail
+      // e.detail is the base64 representation of the JPEG screenshot
+      image.src = 'data:image/jpeg;base64,' + e.detail
 
-            // Show the photo
-            container.classList.add('photo')
-        })
-    }
+      // Show the photo
+      container.classList.add('photo')
+    })
+  }
 })
